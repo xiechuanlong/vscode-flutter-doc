dca84eae72bd8f4933301e1509db19e733d798d3
xiechuanlong <xiechuanlong666@153.com>
2020-12-22 10:37:13 +0800

    Revert "feat: save"
    This reverts commit 86179d6b01cf13bdea9b205d12b60a7bf689318a.

---
+++ extension.js
@@ -0,0 +1,20 @@
+const FlutterDocController = require('./src/flutter_doc_controller');
+const View = require('./src/view');
+const vscode = require('vscode');
+
+const docUrl = 'https://book.flutterchina.club/chapter1/install_flutter.html';
+function activate(context) {
+    vscode.window.showInformationMessage('activity flutter doc plugin!');
+  try {
+    const webView = new View({ context });
+    let esdoc = new FlutterDocController(context, webView, docUrl);
+
+    context.subscriptions.push(esdoc);
+  } catch (error) {
+    console.error(error);
+  }
+}
+exports.activate = activate;
+
+function deactivate() {}
+exports.deactivate = deactivate;
